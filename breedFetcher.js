da090133852ca6415370b0a33af752d66a0dd364
allisonnnnnn <allisonchen1130@gmail.com>
2019-08-01 18:59:00 -0400

    cat API and mocha practice

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
 const request = require("request");
+// const breedName = process.argv[2];
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=&{breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  // request takes 2 parameters(url, callback)
+  request(url, (error, response, body) => {
+    // body is the what returned from request
+    // why need to assign it to data? can we ressign as body? any side effects??
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    let data = JSON.parse(body)[0];
 
-// request takes 2 parameters(url, callback)
-request(url, (error, response, body) => {
-  // body is the what returned from request
-  // why need to assign it to data? can we ressign as body? any side effects??
-  const data = JSON.parse(body);
-  if (error) {
-    // why need return here?
-    return "Error", error;
-  }
+    if (data) {
+      callback(null, data.description.trim());
+    } else {
+      callback("error", null);
+    }
+  });
+};
 
-  // data[0]?? it only return one obj
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  }
-});
+module.exports = { fetchBreedDescription };
